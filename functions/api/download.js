5b3e16908318f21ba8ded513ec6cd9d183239fc0
Nawod Liyanage <130139970+sajika-nawod@users.noreply.github.com>
2026-08-15 20:25:33 +0530

    Update download.js

--- functions/api/download.js
+++ functions/api/download.js
@@ -1,1 +1,20 @@
 POST /api/download
+export async function onRequestPost(context) {
+    const body = await context.request.json();
+
+    const response = await fetch(
+        "https://server.js/download",
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(body)
+        }
+    );
+
+    return new Response(response.body, {
+        status: response.status,
+        headers: response.headers
+    });
+}
